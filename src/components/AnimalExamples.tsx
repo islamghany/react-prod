8317bb0bc05444171bec30141da5948017ded018
islamghany <islamghany3@gmail.com>
2022-04-24 20:30:52 +0200

    add the useApistatus hook to handle different status

--- src/components/AnimalExamples.tsx
+++ src/components/AnimalExamples.tsx
@@ -1,11 +1,19 @@
-import { fetchDog, fetchCat } from '@/api/animalApi'
+import { fetchDog } from '@/api/animalApi'
 import { withAsync } from '@/helpers/withAsync'
 import { useState, useEffect } from 'react'
-import { IDLE, PENDING, SUCCESS, ERROR, ApiStatus } from '@/constants/apiStatus'
+import { PENDING, SUCCESS, ERROR } from '@/constants/apiStatus'
 import LazyLoader from './LazyLoader'
+import { useApiStatus } from '@/hooks/useApiStatus'
 const useFetchDog = () => {
   const [dog, setDog] = useState<string>()
-  const [fetchDogStatus, setFetchDogStatus] = useState<ApiStatus>('IDLE')
+  const {
+    status: fetchDogStatus,
+    setStatus: setFetchDogStatus,
+    isIdle: isFetchDogStatusIdle,
+    isPending: isFetchDogStatusPending,
+    isError: isFetchDogStatusError,
+    isSuccess: isFetchDogStatusSuccess,
+  } = useApiStatus()
   const initFetchDog = async () => {
     setFetchDogStatus(PENDING)
     const { response, error } = await withAsync(() => fetchDog())
@@ -21,40 +29,51 @@ const useFetchDog = () => {
     dog,
     fetchDogStatus,
     initFetchDog,
+    isFetchDogStatusIdle,
+    isFetchDogStatusPending,
+    isFetchDogStatusError,
+    isFetchDogStatusSuccess,
   }
 }
 
-const useFetchCat = () => {
-  const [cat, setCat] = useState<string>()
-  const initFetchCat = async () => {
-    const response = await fetchCat()
-    setCat(response.data?.[0].url)
-  }
-  return {
-    cat,
-    initFetchCat,
-  }
-}
+// const useFetchCat = () => {
+//   const [cat, setCat] = useState<string>()
+//   const initFetchCat = async () => {
+//     const response = await fetchCat()
+//     setCat(response.data?.[0].url)
+//   }
+//   return {
+//     cat,
+//     initFetchCat,
+//   }
+// }
 
-const useFetchAnimals = () => {
-  const { dog, initFetchDog } = useFetchDog()
-  const { cat, initFetchCat } = useFetchCat()
-  const fetchAnimals = () => {
-    initFetchDog()
-    initFetchCat()
-  }
-  useEffect(() => {
-    fetchAnimals()
-  }, [])
-  return {
-    dog,
-    cat,
-    fetchAnimals,
-  }
-}
+// const useFetchAnimals = () => {
+//   const { dog, initFetchDog } = useFetchDog()
+//   const { cat, initFetchCat } = useFetchCat()
+//   const fetchAnimals = () => {
+//     initFetchDog()
+//     initFetchCat()
+//   }
+//   useEffect(() => {
+//     fetchAnimals()
+//   }, [])
+//   return {
+//     dog,
+//     cat,
+//     fetchAnimals,
+//   }
+// }
 
 function AnimalExampleWithApiStates() {
-  const { dog, fetchDogStatus, initFetchDog } = useFetchDog()
+  const {
+    dog,
+    initFetchDog,
+    isFetchDogStatusError,
+    isFetchDogStatusIdle,
+    isFetchDogStatusPending,
+    isFetchDogStatusSuccess,
+  } = useFetchDog()
   useEffect(() => {
     initFetchDog()
   }, [])
@@ -62,10 +81,10 @@ function AnimalExampleWithApiStates() {
     <div className="my-8 mx-auto max-w-2xl">
       <div className="flex justify-center gap-8">
         <div className="w-64 h-64">
-          {fetchDogStatus === IDLE ? <p>Welcome</p> : null}
-          <LazyLoader show={fetchDogStatus === PENDING} delay={400} />
-          {fetchDogStatus === ERROR ? <p>There was a problem</p> : null}
-          {fetchDogStatus === SUCCESS ? (
+          {isFetchDogStatusIdle ? <p>Welcome</p> : null}
+          <LazyLoader show={isFetchDogStatusPending} delay={400} />
+          {isFetchDogStatusError ? <p>There was a problem</p> : null}
+          {isFetchDogStatusSuccess ? (
             <img className="h-64 w-full object-cover" src={dog} alt="Dog" />
           ) : null}
         </div>
